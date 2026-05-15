68d9a5a01d82db95c3d21c2fbd83d44e397355d7
raphael.petet.thozet <raphael.petet.thozetplebeaucours.fr>
2026-05-15 21:56:44 +0200

    essai généalogie

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from pandas._config import dates
 class Indi:
     def __init__(self, id, c, nom, dadtab, momtab):
         self.nom=nom
+        self.papa=str(dadtab)
+        self.mom=str(momtab)
         self.id=id
         self.c=c
         randomtab=[[random.randint(0, 1) for j in range(self.c)] for i in range(self.c)]
@@ -24,8 +26,18 @@ class Indi:
         pass
 
     def affiche(self):
+        print(self.nom)
         for i in self.tab:
             print(str(i))
+        print("\n")
+    
+    def adn(self):
+        return self.tab
+    
+    # essai d'un bidule pour avoir en mémoire les parents d'un gugs, foireux. Faut voir comment prendre les noms des générations antérieures à l'individu
+    def parents(self):
+        print(self.papa + self.maman)
+
 
 
 c = 6
@@ -44,8 +56,29 @@ for i in momtab_robert:
 
 print("\n")
 
+print("momtab_roberta")
+momtab_roberta=[[random.randint(0, 1) for j in range(c)] for i in range(c)]
+for i in momtab_robert:
+    print(str(i))
+
+print("\n")
+
+print("dadtab_roberta")
+dadtab_roberta=[[random.randint(0, 1) for j in range(c)] for i in range(c)]
+for i in momtab_robert:
+    print(str(i))
+
+print("\n")
+
 people=Indi(0, c, "Robert",dadtab_robert, momtab_robert)
 people.affiche()
+
+pine_apple=Indi(1, c, "Roberta", dadtab_roberta, momtab_roberta)
+pine_apple.affiche()
+
+apple=Indi(1, c, "Robierto", people.adn(), pine_apple.adn())
+apple.affiche()
+
 
 
 #people2=Indi(0, 3, "Roberta")

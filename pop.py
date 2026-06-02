c1775d23371a0cde786f91194220958961e435b2
Pittermanifique <isaac.lestage@gmail.com>
2026-06-02 16:28:03 +0200

    base interactions

--- pop.py
+++ pop.py
@@ -2,19 +2,39 @@ import indi
 import random
 import copy
 
-
-class pop:
-    def __init__(self):
+class Pop:
+    def __init__(self,c):
         self.individus = []
         self.liste_interactions = []
         self.gen = 0
+        self.c = c
 
-    def gen_base_pop(self, n_pepoles, c):
+    def gen_base_pop(self, n_pepoles):
         if len(self.individus) == 0:
             for i in range(n_pepoles):
-                new_indi = indi.Indi(i,c)
+                new_indi = indi.Indi(i,self.c)
                 new_indi.new_base_indi()
                 self.individus.append(new_indi)
+
+    def new_interactions(self,id1,id2,interaction_type):
+        if interaction_type == "same":
+            random_col = random.randint(0,self.c-1)
+            random_line = random.randint(0,self.c-1)
+            interaction = (id1,id2,interaction_type,(random_line,random_col))
+            self.liste_interactions.append(interaction)
+            print(interaction)
+
+    def update_interactions(self):
+        new_individus = copy.deepcopy(self.individus)
+        for i in self.liste_interactions:
+            id1 = i[0]
+            id2 = i[1]
+            interaction_type = i[2]
+            if interaction_type == "same":
+                coo = i[3]
+                target = new_individus[id2]
+                base = self.individus[id1]
+                target.tab[coo[0]][coo[1]] = base.tab[coo[0]][coo[1]]
 
     def update_pop(self):
         for i in self.individus:
@@ -27,9 +47,13 @@ class pop:
             print("\n")
 
 if __name__ == "__main__":
-    population = pop()
-    population.gen_base_pop(1,10)
+    population = Pop(10)
+    population.gen_base_pop(2)
     population.affiche()
-    population.update_pop()
+
+    for i in range(10):
+        population.new_interactions(0, 1, "same")
+
+    population.update_interactions()
     population.affiche()
 
